e1f1ceb6a1fd08d532b249f6ffa14b1e5ed75934
ricky-kiva <ricky.kiva@gmail.com>
2023-07-14 14:48:23 +0700

    add notification function

--- app/src/main/java/com/dicoding/habitapp/notification/NotificationWorker.kt
+++ app/src/main/java/com/dicoding/habitapp/notification/NotificationWorker.kt
@@ -1,11 +1,20 @@
 package com.dicoding.habitapp.notification
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.dicoding.habitapp.R
+import com.dicoding.habitapp.ui.detail.DetailHabitActivity
 import com.dicoding.habitapp.utils.HABIT_ID
 import com.dicoding.habitapp.utils.HABIT_TITLE
+import com.dicoding.habitapp.utils.NOTIFICATION_CHANNEL_ID
+import com.dicoding.habitapp.utils.NOTIF_UNIQUE_WORK
 
 class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
 
@@ -16,9 +25,43 @@ class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, p
         val prefManager = androidx.preference.PreferenceManager.getDefaultSharedPreferences(applicationContext)
         val shouldNotify = prefManager.getBoolean(applicationContext.getString(R.string.pref_key_notify), false)
 
-        //TODO 12 : If notification preference on, show notification with pending intent
+        // XTODO 12 : If notification preference on, show notification with pending intent
+        if (shouldNotify) {
+            showNotification()
+        }
 
         return Result.success()
     }
 
+    private fun showNotification() {
+        val notificationId = habitId
+
+        val notificationManager =
+            applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Create the notification channel for Android Oreo and above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                NOTIF_UNIQUE_WORK,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val intent = Intent(applicationContext, DetailHabitActivity::class.java).apply {
+            putExtra(HABIT_ID, habitId)
+        }
+        val pendingIntent = PendingIntent.getActivity(applicationContext, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val notification = NotificationCompat.Builder(applicationContext, NOTIFICATION_CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_notifications)
+            .setContentTitle(habitTitle)
+            .setContentText(applicationContext.getString(R.string.notify_content))
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .build()
+
+        notificationManager.notify(notificationId, notification)
+    }
 }
