200e3cba5a238229fa78466ff0ac270bcf89dca2
ricky-kiva <ricky.kiva@gmail.com>
2023-07-14 14:20:18 +0700

    complete Habit Adapter

--- app/src/main/java/com/dicoding/habitapp/ui/list/HabitAdapter.kt
+++ app/src/main/java/com/dicoding/habitapp/ui/list/HabitAdapter.kt
@@ -1,5 +1,6 @@
 package com.dicoding.habitapp.ui.list
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -14,33 +15,49 @@ class HabitAdapter(
     private val onClick: (Habit) -> Unit
 ) : PagedListAdapter<Habit, HabitAdapter.HabitViewHolder>(DIFF_CALLBACK) {
 
-    //TODO 8 : Create and initialize ViewHolder
+    // XTODO 8 : Create and initialize ViewHolder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HabitViewHolder {
-        throw NotImplementedError("Not yet implemented")
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.habit_item, parent, false)
+        return HabitViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: HabitViewHolder, position: Int) {
-        //TODO 9 : Get data and bind them to ViewHolder
+        val habit = getItem(position)
+        habit?.let { holder.bind(it) }
     }
 
     inner class HabitViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         private val tvTitle: TextView = itemView.findViewById(R.id.item_tv_title)
-        val ivPriority: ImageView = itemView.findViewById(R.id.item_priority_level)
+        private val ivPriority: ImageView = itemView.findViewById(R.id.item_priority_level)
         private val tvStartTime: TextView = itemView.findViewById(R.id.item_tv_start_time)
         private val tvMinutes: TextView = itemView.findViewById(R.id.item_tv_minutes)
 
         lateinit var getHabit: Habit
+
+        init {
+            itemView.setOnClickListener {
+                if (::getHabit.isInitialized) {
+                    onClick(getHabit)
+                }
+            }
+        }
+
         fun bind(habit: Habit) {
             getHabit = habit
             tvTitle.text = habit.title
             tvStartTime.text = habit.startTime
             tvMinutes.text = habit.minutesFocus.toString()
-            itemView.setOnClickListener {
-                onClick(habit)
-            }
-        }
 
+            val priorityDrawable = when (habit.priorityLevel) {
+                "High" -> R.drawable.ic_priority_high
+                "Medium" -> R.drawable.ic_priority_medium
+                "Low" -> R.drawable.ic_priority_low
+                else -> R.drawable.ic_priority_low
+            }
+
+            ivPriority.setImageResource(priorityDrawable)
+        }
     }
 
     companion object {
